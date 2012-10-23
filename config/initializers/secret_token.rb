c1055d54d0ed9b2501d235279a1dc80128b28847
Shanks <shanks@ubuntu.ubuntu-domain>
2012-10-23 21:19:10 +0300

    index moific...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PupazaRobert::Application.config.secret_token = 'c41cf6b914968cb94e18770b59518defea049b5af36f1bb7c0712385d1b34171f43d7028d57fe6b4c4c9943700aa1a2937f09119e9b8fc42883cceb115a8efe7'
